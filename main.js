2b4595e74d081050ba1d55e021016bd5c5936be9
JonathanD1802 <jonathan_delgrange@yahoo.fr>
2024-04-17 12:48:59 +0200

    Commentary code finished

--- main.js
+++ main.js
@@ -15,7 +15,28 @@ const mockUpStrand = () => {
 
 /* 
 currentOrganismNumber and PAequorFactory function are associated. 
+CurrentOrganismNumber variable will keep track of the Current specimen number ; so that two specimens won't have the same number. 
+CurrentOrganismeNumber is passed as an argument to the PAequorFactory function so that each time the function is called, the number is 
+increased by one. 
 
+the PAequorFactory function return an specimen's object that has 2 properties and 3 methods. 
+The properties give us the specimen number that is generate and a random DNA with 15 bases with the mockUpStrand() passed as parameters.
+
+the mutate() method take a random DNA Base from the dna propertie, and generate a new DNA base with the returnRandBase function until
+the new DNA generated and the current DNA from the propertie are different. 
+Then, it replaces the current DNA with the new one generate. to simulate the high rate of mutation.
+finally it return the dna properties with the new base generate. 
+
+ComparePAequor() method take another specimen as parameter. 
+this function aims to compare the both dna properties of the two specimens. 
+Each time a DNA base are equal and are at the same index, the function increase identicalDNABase by one. 
+Then it prints to the user the percentage both specimens have in common if the formula (IndenticalDNABase * 100)/dna.length. 
+
+The final methods willLikeliSurvive() computes the percentage of chance a specimen has to survive. 
+it iterate through the dna array and increase the survivePercentage from 1 each time a specimen dna is equal to "C" or "G". 
+Then, if the Math.round() methods it convert the survivePercentage into percentage. 
+Finally if the percentage is equal or above 60%, it returns true. 
+Otherwise, it returns false. 
 */
 let currentOrganismNumber = 1; 
 const PAequorFactory = (number = currentOrganismNumber,DNAbases = mockUpStrand() ) => {
@@ -59,8 +80,11 @@ const PAequorFactory = (number = currentOrganismNumber,DNAbases = mockUpStrand()
 }; 
   
 
+//Specimens array that will contains 30 specimens that have greater chance to survive. 
 let specimens = []; 
 
+//The while loop create specimens and computes the chances a specimen has to survive, the if it has greater chance, 
+//it push the specimen into the specimens array. 
 while(specimens.length < 30){
   let specimenTest = PAequorFactory(); 
   if(specimenTest.willLikelySurvive()){
